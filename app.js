26f33fabe4523c489e8b17cbdb54c8bf4098f4fc
sandyweboo <102416086+sandyweboo@users.noreply.github.com>
2023-11-20 07:28:20 +0530

    untill express routing params

---
+++ app.js
@@ -0,0 +1,50 @@
+const bodyParser = require("body-parser");
+const express = require("express");
+const app = express();
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({extended : false}));
+app.set('view engine', 'ejs');
+
+const blogPosts = [];
+
+const homePost = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas eget justo nec nisi sollicitudin aliquam. Nullam fringilla auctor nisi, ac rhoncus urna dignissim id. Suspendisse potenti. Sed eu augue vitae lacus efficitur viverra. Integer id urna vel eros commodo vulputate. Vivamus vel turpis ut orci placerat ultricies.";
+
+const about = "Welcome to [Your Company/Personal Name]! We are dedicated to [briefly describe your mission or purpose]. With a passionate team of experts, we strive to [mention a key goal or achievement]. Join us on this exciting journey as we continue to [highlight a unique aspect of your business or mission].";
+
+const contact ="Have a question or feedback? We're here to help! Feel free to reach out to us via the contact form or directly at [Your Email Address]. Your input is valuable to us, and we look forward to connecting with you. For urgent inquiries, you can also reach us by phone at [Your Phone Number].";
+
+
+app.get("/",(req, res)=>{
+
+   
+    res.render("home",{content :homePost, article : blogPosts });
+});
+
+app.get("/about",(req,res)=>{
+   
+    res.render("about",{content :about})
+   
+})
+app.get("/contact",(req,res)=>{
+    res.render("contact",{content :contact })
+})
+app.get("/compose",(req,res)=>{
+    res.render("compose",{ })
+  
+})
+app.post("/compose",(req,res)=>{
+  
+const  post ={ 
+    title : req.body.postTitle,
+    content : req.body.postBody
+}
+
+blogPosts.push(post);
+res.redirect('/');   
+})
+app.get("/post/:postName",(req,res)=>{
+console.log(req.params.postName);
+})
+app.listen(process.env.port || 3000,()=>{
+    console.log("app is runnig on port 3000");
+})
